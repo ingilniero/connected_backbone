3a28cfe7f31971bca3598fadf706bbf0d1f6dc5a
Gilberto Villa <ingilniero@gmail.com>
2013-06-29 15:32:03 -0500

    Inline Template

--- js/main.js
+++ js/main.js
@@ -2,26 +2,25 @@ var Person = Backbone.Model.extend({
   defaults: {
     name: 'John Doe',
     age: 20,
-    ocuppation: 'worker'
+    occupation: 'worker'
   }
 });
 
 
 var PersonView = Backbone.View.extend({
   tagName: 'li',
-  id: 'person-id',
-  className: 'person',
+
+  template: _.template("<strong><%= name %></strong> (<%= age %>) - <%= occupation%>"),
 
   initialize: function(){
     this.render();
   },
 
   render: function(){
-    // anti-pattern
-    this.$el.html(this.model.get('name') + '('+this.model.get('age')+") - "+this.model.get('ocuppation'));
+    this.$el.html(this.template(this.model.toJSON()));
   }
 
 });
 
-var person = new Person()
+var person = new Person;
 var personView = new PersonView({ model: person });
