618d2337ddced198b877d56e96ea74d8c0f581b7
Gilberto Villa <ingilniero@gmail.com>
2013-06-29 14:25:13 -0500

    First Model

--- js/main.js
+++ js/main.js
@@ -1,10 +1,21 @@
-var Person = function(config) {
-  this.name = config.name;
-  this.age = config.age;
-  this.ocuppation = config.ocuppation;
-};
+var Person = Backbone.Model.extend({
+  defaults: {
+    name: 'John Doe',
+    age: 20,
+    ocuppation: 'worker'
+  },
 
+  work: function(){
+    return this.get('name') + ' is working';
+  }
+});
 
-Person.prototype.work = function() {
-  return this.name + " is working";
-};
+// var Person = function(config) {
+//   this.name = config.name;
+//   this.age = config.age;
+//   this.ocuppation = config.ocuppation;
+// };
+
+// Person.prototype.work = function() {
+//   return this.name + " is working";
+// };
