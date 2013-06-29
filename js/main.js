17880e6ad373f31e701a8364d5004315a79748d2
Gilberto Villa <ingilniero@gmail.com>
2013-06-29 17:12:43 -0500

    Template Helper

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+var template = function(id) {
+  return _.template( $('#' + id).html() );
+};
+
 // Person Model
 var Person = Backbone.Model.extend({
   defaults: {
@@ -32,7 +36,7 @@ var PeopleView = Backbone.View.extend({
 var PersonView = Backbone.View.extend({
   tagName: 'li',
 
-  template: _.template($('#personTemplate').html()),
+  template: template('personTemplate'),
 
   render: function(){
     this.$el.html(this.template(this.model.toJSON()));
