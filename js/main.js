2d96ed2a8b6d6ad2b4a4cbdf07bdb217c3b866a4
Gilberto Villa <ingilniero@gmail.com>
2013-06-29 18:00:37 -0500

    Tasks setup

--- js/main.js
+++ js/main.js
@@ -1,7 +1,56 @@
 (function(){
+
   window.App = {
     Models: {},
     Collections: {},
     Views: {}
   };
+
+  window.template = function(id) {
+    return _.template( $('#' + id).html() );
+  };
+
+  App.Models.Task = Backbone.Model.extend({});
+
+  App.Collections.Tasks = Backbone.Collection.extend({
+    model: App.Models.Task
+  });
+
+  App.Views.Tasks = Backbone.View.extend({
+    tagName: 'ul',
+
+    render: function(){
+      this.collection.each(this.addOne, this);
+      return this;
+    },
+
+    addOne: function(task){
+      taskView = new App.Views.Task( { model: task });
+      this.$el.append(taskView.render().el);
+    }
+  });
+
+  App.Views.Task = Backbone.View.extend({
+    tagName: 'li',
+
+    render: function(){
+      this.$el.html( this.model.get('title') );
+      return this;
+    }
+  });
+
+  //Data
+  var tasksCollection = new App.Collections.Tasks([
+    {
+      title: 'Go to the store',
+      priority: 4
+    },
+    {
+      title: 'Go to the mall',
+      priority: 3
+    }
+  ]);
+
+  var tasksView = new App.Views.Tasks({ collection: tasksCollection });
+  $('.tasks').append(tasksView.render().el);
 })();
