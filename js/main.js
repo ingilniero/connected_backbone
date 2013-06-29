e863277a0993ce1bc9f7c27071fd8f10aff697f0
Gilberto Villa <ingilniero@gmail.com>
2013-06-29 15:58:15 -0500

    Collections

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// Person Model
 var Person = Backbone.Model.extend({
   defaults: {
     name: 'John Doe',
@@ -6,7 +7,12 @@ var Person = Backbone.Model.extend({
   }
 });
 
+// A list of people
+var PeopleCollection = Backbone.Collection.extend({
+  model: Person
+});
 
+// The View for a Person
 var PersonView = Backbone.View.extend({
   tagName: 'li',
 
@@ -22,5 +28,23 @@ var PersonView = Backbone.View.extend({
 
 });
 
+// Data
 var person = new Person;
 var personView = new PersonView({ model: person });
+
+var peopleCollection = new PeopleCollection([
+    {
+      name: 'Aris',
+      age: 25
+    },
+    {
+      name: 'Alba',
+      age: 10
+    }
+  ]);
+
+peopleCollection.add(person);
+
+var person2 = new Person({ name: 'Gilberto Villa', age: 22 });
+var personView2 = new PersonView({model: person2});
+peopleCollection.add(person2);
