67eb5695ca18e01d68fc61cee8e6a1bbf6cd0fb6
Gilberto Villa <ingilniero@gmail.com>
2013-06-29 15:24:17 -0500

    tagName, id, className properties. Initiaize and render methods

--- js/main.js
+++ js/main.js
@@ -3,20 +3,25 @@ var Person = Backbone.Model.extend({
     name: 'John Doe',
     age: 20,
     ocuppation: 'worker'
-  },
-
-  //Called on object.(object, {validate: true} ) or save()
-  validate: function( attrs, options ) {
-    if (attrs.age < 0 ){
-      return "Age must be positive.";
-    }
-
-    if(!attrs.name ){
-      return "Every person must have a name.";
-    }
-  },
-
-  work: function(){
-    return this.get('name') + ' is working';
   }
 });
+
+
+var PersonView = Backbone.View.extend({
+  tagName: 'li',
+  id: 'person-id',
+  className: 'person',
+
+  initialize: function(){
+    this.render();
+  },
+
+  render: function(){
+    // anti-pattern
+    this.$el.html(this.model.get('name') + '('+this.model.get('age')+") - "+this.model.get('ocuppation'));
+  }
+
+});
+
+var person = new Person()
+var personView = new PersonView({ model: person });
