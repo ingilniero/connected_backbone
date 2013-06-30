c7023ce4350f42e3dd3b6973276178586cf1299a
Gilberto Villa <ingilniero@gmail.com>
2013-06-29 22:59:44 -0500

    Triggering Custom Events

--- js/main.js
+++ js/main.js
@@ -7,36 +7,89 @@
     Router: {}
   };
 
+  var clear = function(){
+    $(document.body).html('');
+  };
+
+  var vent = _.extend({}, Backbone.Events);
+
+  App.Models.Appointment = Backbone.Model.extend({
+  });
+
+  App.Collections.Appointments = Backbone.Collection.extend({
+    model: App.Models.Appointment
+  });
+
+  App.Views.Appointment = Backbone.View.extend({
+    tagName: 'h1',
+
+    template: _.template("<a href='#appointment/<%= id %>'><%= title %></a>"),
+
+    render: function(){
+      this.$el.html(this.template(this.model.toJSON()));
+      return this;
+    }
+  });
+
+  App.Views.Appointments = Backbone.View.extend({
+    tagName: 'ul',
+
+    initialize: function(){
+      vent.on('appointment:show', this.showAppointment, this);
+      vent.on('appointments:index', this.showAppointments, this);
+    },
+
+    render: function(){
+      this.collection.each(this.addAppointment, this);
+      return this;
+    },
+
+    showAppointments: function(){
+      clear();
+      $(document.body).append(this.render().el);
+    },
+
+    addAppointment: function(appointment){
+      var appointmentView = new App.Views.Appointment({ model: appointment});
+      this.$el.append( appointmentView.render().el);
+    },
+
+    showAppointment: function( id ) {
+      var appointment = this.collection.get(id);
+      var appointmentView = new App.Views.Appointment({ model: appointment });
+      clear();
+      $(document.body).append(appointmentView.render().el);
+    }
+  });
+
   App.Router = Backbone.Router.extend({
     routes: {
       '': 'index',
-      'show/:id': 'show',
-      'download/*filename':'download',
-      'search/:query': 'search',
-      '*other': 'default'
+      'appointment/:id': 'showAppointment'
     },
 
     index: function(){
-      console.log("hello from index");
+      vent.trigger('appointments:index');
     },
 
-    show: function(id){
-      console.log("show route for "+ id);
-    },
-
-    download: function(filename){
-      console.log("downloading..."+ filename);
-    },
-
-    search: function(query){
-      console.log(query);
-    },
-
-    default: function(other){
-      alert('Hmmmmm...not sure of what you want');
+    showAppointment: function(appointmentId){
+      vent.trigger('appointment:show', appointmentId);
     }
 
   });
+
+  var appointments = new App.Collections.Appointments([
+    {
+      id: 1,
+      title: 'Appointment 1'
+    },
+    {
+      id: 2,
+      title: 'Appointment 2'
+    }
+    ]);
+
+  var appointmentsView = new App.Views.Appointments({collection: appointments});
 
   new App.Router;
   Backbone.history.start();
